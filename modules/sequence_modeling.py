ddffeeb1b6ef3836d235d4f3bf0531882d454e84
Asher Mancinelli <asher.mancinelli@pnnl.gov>
2019-07-15 10:41:44 -0700

    adding my rosetta module

--- modules/sequence_modeling.py
+++ modules/sequence_modeling.py
@@ -17,3 +17,33 @@ class BidirectionalLSTM(nn.Module):
         recurrent, _ = self.rnn(input)  # batch_size x T x input_size -> batch_size x T x (2*hidden_size)
         output = self.linear(recurrent)  # batch_size x T x output_size
         return output
+
+def res_block(in_chans, out_chans, kernel_size=(3, 3)
+                strides=((1, 1), (1, 1)):
+    return nn.Sequential(
+            nn.Conv2d(
+                in_channels=in_chans,
+                out_channels=out_chans,
+                kernel_size=kernel_size,
+                strides=strides[0]
+            ),
+            nn.Conv2d(
+                in_channels=in_chans,
+                out_channels=out_chans,
+                kernel_size=kernel_size,
+                strides=strides[1]
+            )
+        )
+
+class Rosetta(nn.Module):
+
+    def __init__(self, input_size, hidden_size, output_size):
+        super(Rosetta, self).__init__()
+
+    def forward(self, input):
+        x = self.res_block_1(x)
+        x = self.res_block_2(x)
+        x = self.res_block_3(x)
+        x = self.res_block_4(x)
+        x = self.last_conv(x)
+        x = self.log(x)
